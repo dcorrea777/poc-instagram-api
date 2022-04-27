0195320c762ce7db1c6c5d0fcf91402a3055286b
weliton <welingtonzsilva@gmail.com>
2022-04-27 17:05:17 -0300

    verifica  verifica se a senha tem entre 8 e 16 caracteres         verifica se a senha tem pelo menos uma letra maiuscula e um numero

--- src/Domain/Entities/ValueObjects/Password.php
+++ src/Domain/Entities/ValueObjects/Password.php
@@ -14,13 +14,17 @@ final class Password
             $this->pass = md5($pass);
         }   
     }
-
+      /*
+        verifica se a senha tem entre 8 e 16 caracteres
+        verifica se a senha tem pelo menos uma letra maiuscula e um numero
+     */ 
     private function valitadePass(string $pass):bool
     {
-        if(strlen($pass) >= 8){
+        if(preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[\w$@]{8,16}$/', $pass)){
             return true;
         }else{
-            throw new Exception('Password is not valid');
+            throw new Exception('A senha deve conter entre 8 e 16
+             caracteres e ter pelo menos 1 letra Maiscula e numero');
             return false;
         }
     }
